837101c387143a1af5a41b030f21697537e66694
Elvinn <elvinn.tombert@hetic.net>
2017-11-06 00:47:07 +0100

    end

--- app/dist/scripts/main.js
+++ app/dist/scripts/main.js
@@ -2,3 +2,59 @@ $(document).ready(function(){
 	// alert('hello');
 });
 
+
+$(document).ready(function(){
+	var swiper = new Swiper('.swiper-container', {
+		pagination: {
+			el: '.swiper-pagination',
+			dynamicBullets: true,
+		},
+		autoplay: {
+			delay: 5000,
+		},
+	});
+	
+});
+
+
+$(document).ready(function(){
+	$('.search').click(function(e){
+		e.preventDefault();
+		$('.search').toggleClass('active');
+		$('.search-bar input').slideToggle('down');
+	});
+});
+
+
+$(document).ready(function(){
+	$(".burger-menu").click(function () {
+		$(this).toggleClass("menu-on");
+		$('.nav-items').animate({width:'toggle'},350);
+	});
+});
+
+
+$(document).ready(function() {
+    
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.hideme').each( function(i){
+            
+            var bottom_of_object = $(this).offset().top -500 + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                // $(this).animate({'opacity':'1'},500);
+                $(this).addClass('showme');
+                    
+            }
+            
+        }); 
+    
+    });
+    
+});
